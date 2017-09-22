694d2018ccd82f5b03ec614a8a0fc3249822f182
Mann <cmmann@iastate.edu>
2017-09-22 15:44:38 -0500

    Added validation checks on genbank id and input copy/paste gene sequence, added functions for processing exon inpt, changed output format to DT:dataTable, updated example pre-populate function to adjust inputType radio button

--- server.R
+++ server.R
@@ -5,25 +5,130 @@ library(shinyIncubator)
 library(stringr)
 library(stringi)
 library(Biostrings)
+library(DT)
 
 source("requiredFunctions.R")
 
 shinyServer(function(input, output, session){
-	output$exonInfo <- renderRHandsontable({
-		df = data.frame(exonStart = numeric(5), exonEnd = numeric(5), stringsAsFactors = FALSE)
-		rhandsontable(df) %>%
-			hot_context_menu(allowRowEdit = TRUE, allowColEdit = FALSE) %>%
-			hot_col("exonStart", format = "0") %>%
-			hot_col("exonEnd", format = "0")
+	####Validation Checks####
+	
+	####Make sure GenBank/RefSeq ID is properly formatted####
+	validGenBankId <- reactive({
+		#If input type is genbank/refseq, and the string is not empty
+		if((input$inputType == 1) && (input$genbankId != "")){
+			#If the input starts off with two characters followed by an underscore
+			if(str_detect(toupper(input$genbankId), "[A-Za-z0-9]{2}\\_")){
+				#If the input has more than one underscore
+				if(str_detect(toupper(input$genbankId), "[\\_].*[\\_]")){
+					validate(
+						need(!str_detect(toupper(input$genbankId), "[\\_].*[\\_]"),
+								 "Error: This accession contains two underscores. RefSeq accession IDs contain only a single underscore.")
+					)
+				} else {
+					validate(
+						#If the sequence contains anything other than alphanumerics, underscores, or periods
+						need(!str_detect(toupper(input$genbankId), "[^\\w\\.]"),
+								 "Error: The accession contains restricted characters. Accession IDs may contain only letters, numbers, periods, and a single underscore ('_') after the first two characters, in the case of RefSeq IDs."
+						))
+				}
+				
+			} else {
+				#If a genbank accession
+				validate(
+					need(!str_detect(toupper(input$genbankId), "[^A-Za-z0-9\\.]"),
+							 "Error: The accession contains restricted characters. Accession IDs may contain only letters, numbers, periods, and a single underscore ('_') after the first two characters, in the case of RefSeq IDs."
+					)
+				)
+			}
+			
+		} 
 	})
 	
+	####Validate copy/paste sequence input####
+	validGeneSeq <- reactive({
+		#If the input type is copy/paste gene seq and text has been entered
+		if((input$geneSeq != "") && (input$inputType == 2)){
+			validate(
+				#Check for fasta format
+				need(!str_detect(input$geneSeq, "[>]"), 
+						 "Error: Input DNA sequence appears to be in FASTA format. Please paste your sequence without the fasta header."),
+				#Check for not DNA input
+				need(!str_detect(input$geneSeq, "[^ACGTacgt0-9\\s\\n]"), 
+						 paste0("Error: Input DNA sequence contains non-standard nucleotides. ", 
+						 			 "Allowed nucleotides are A, C, G, and T.")),
+				need(nchar(input$geneSeq) < 5000, "The DNA sequence has >5000 nucleotides. For sequences of this size, please use the local version of MENTHU, which can be accessed via the 'Tools and Downloads' tab.")
+			)
+		} else if(input$inputType == 1) {
+			#prevent crash on empty submission
+			validate(
+				need(input$geneSeq != "", "")
+			)
+		}
+	})
+	
+	#Exon input validation
+	validExonInfo <- reactive({
+		if(!is.null(input$exonInfo)){
+			values[["old"]] <- isolate(values[[""]])
+		}
+	})
+	
+	#Render exon table
+	output$exonInfo <- renderRHandsontable({
+		if(!is.null(input$exonInfo)){
+			exonDF = hot_to_r(input$exonInfo)
+		} else {
+			exonDF = data.frame(exonStart = numeric(5), exonEnd = numeric(5), stringsAsFactors = FALSE)
+		}
+		
+			rhandsontable(exonDF) %>%
+				hot_context_menu(allowRowEdit = TRUE, allowColEdit = FALSE) %>%
+				hot_col("exonStart", format = "0") %>%
+				hot_col("exonEnd", format = "0")
+		
+	})
+	
+	
+	########################################################
+	##############PRINT VALIDATION RESULTS##################
+	########################################################
+	output$validgenbankid <- renderText({
+		validGenBankId()
+	})
+	
+	output$genbankidexists <- renderText({
+		
+	})
+	
+	output$validgeneseq <- renderText({
+		validGeneSeq()
+	})
+	
+	#output$validexoninfo <- renderText({
+	#	validExonInfo()
+	#})
+	
+	###Exon Input Table Handler####
+	#observe({
+	#	if(!is.null(input$exonInfo)){
+	#		values[["prev"]] <- isolate(values[["exonDF"]])
+	#		exonDF = hot_to_r(input$exonInfo)
+	#	} else {
+	#		if(is.null(values[["exonDF"]])){
+	#			exonDF <- exonDF
+	#		} else {
+	#			exonDF <- values[["exonDF"]]
+	#		}
+	#	}
+	#	values[["exonDF"]] <- exonDF
+	#})
 	
 	####Update checkbox with select all####
 	observeEvent(input$selectAll, {
 		
 		updateCheckboxGroupInput(session, 
 														 "casType", 
-														 label = "",
+														 label = "1. Select the PAM sequence(s) you wish to target:",
 														 choices = list("S. pyogenes SpCas9: 5'-NGG-3'" = "NGG",
 														 							 "S. pyogenes SpCas9: 5'-NRG-3'" = "NRG",
 														 							 "S. aureus SaCas9: 5'-NNNRRT-3'" = "NNNRRT",
@@ -42,7 +147,7 @@ shinyServer(function(input, output, session){
 	observeEvent(input$selectNone, {
 		updateCheckboxGroupInput(session, 
 														 "casType", 
-														 label = "",
+														 label = "1. Select the PAM sequence(s) you wish to target:",
 														 choices = list("S. pyogenes SpCas9: 5'-NGG-3'" = "NGG",
 														 							 "S. pyogenes SpCas9: 5'-NRG-3'" = "NRG",
 														 							 "S. aureus SaCas9: 5'-NNNRRT-3'" = "NNNRRT",
@@ -61,16 +166,32 @@ shinyServer(function(input, output, session){
 
 	#Submit Handler
 	observeEvent(input$geneSeqSubmit, {
-		results <- calculateMENTHU(input$casType, input$geneSeq, input$threshold)
-		results <- results[order(-results$menthuScore, results$toolType, results$location),]
-		print(results)
-	output$geneSeqResults <- renderTable(results)
-	  
-	  
+			if(input$pasteExonType == 0){
+				exons <- 0
+			} else {
+				exons <- exonHandler(input$exonInfo)
+				if(nrow(exons) == 0){
+					exons <- 0
+				}
+			}
+			print(exons)
+			
+			results <- calculateMENTHUGeneSeq(input$casType, input$geneSeq, input$threshold, exons)
+			results <- results[order(-results$menthuScore, results$toolType, results$location),]
+			#print(results)
+			output$geneSeqResults <- DT::renderDataTable(results, options = list(scrollX = TRUE))
+		
 	})
+
 	
-	observeEvent(input$example, {
+	observeEvent(input$exampleGeneSeq, {
 		#reset()
+		updateRadioButtons(session,
+											 "inputType",
+											 label = "2. What type of sequence input do you want to use?",
+											 choices = list("GenBank Gene ID"  = 1,
+											 							 "Copy/paste gene sequence" = 2),
+											 selected = 2)
 		updateTextAreaInput(session, "geneSeq", label = "", value = "CTTTCCTGCGTTATCCCCTGATTCTGTGGATAACCGTATTACCGCCTTTGAGTGAGCTGATACCGCTCGCCGCAGCCGAACGACCGAGCGCAGCGAGTCAGTGAGCGAGGAAGCGGAAGAGCGCCCAATACGCAAACCGCCTCTCCCCGCGCGTTGGCCGATTCATTAATGCAGCTGGCACGACAGGTTTCCCGACTGGAAAGCGGGCAGTGAGCGCAACGCAATTAATACGCGTACCGCTAGCCAGGAAGAGTTTGTAGAAACGCAAAAAGGCCATCCGTCAGGATGGCCTTCTGCTTAGTTTGATGCCTGGCAGTTTATGGCGGGCGTCCTGCCCGCCACCCTCCGGGCCGTTGCTTCACAACGTTCAAATCCGCTCCCGGCGGATTTGTCCTACTCAGGAGAGCGTTCACCGACAAACAACAGATAAAACGAAAGGCCCAGTCTTCCGACTGAGCCTTTCGTTTTATTTGATGCCTGGCAGTTCCCTACTCTCGCGTTAACGCTAGCATGGATGTTTTCCCAGTCACGACGT")
 		updateCheckboxGroupInput(session, 
 														 "casType", 
